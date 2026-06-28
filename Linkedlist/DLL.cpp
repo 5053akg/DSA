4a8be5a34d9f7204badb163361d8c5054b3c5b0d
akg5053 <akg5053@gmail.com>
2026-06-28 14:00:39 +0530

    continues

--- Linkedlist/DLL.cpp
+++ Linkedlist/DLL.cpp
@@ -66,9 +66,108 @@ Node* deleteTail(Node* head){
     newTail->next = nullptr;
     tail->back = nullptr;
     
-    delete tail;
+    delete tail; 
     return head;
 
+}
+
+Node* removeKthElement(Node* head, int k){
+    if(head == NULL){
+        return NULL;
+    }
+    int cnt = 0;
+    Node* kNode = head;
+    while(kNode!=NULL){
+        cnt++;
+        if(cnt == k) break;
+        kNode = kNode->next;
+    }
+    Node* prev = kNode->back;
+    Node* front = kNode->next;
+
+    if(prev==NULL && front == NULL){
+        return NULL;
+    }
+    else if(prev == NULL){
+        return deleteHead(head);
+    }
+    else if(front == NULL){
+        return deleteTail(head);
+    }
+    prev->next = front;
+    front->back = prev;
+
+    kNode->next = kNode->back = nullptr;
+    delete kNode;
+    return head;
+}
+
+
+void deleteNode(Node* temp){
+    Node* prev = temp->back;
+    Node* front = temp->next;
+
+    if(front == NULL){
+        prev->next = nullptr;
+        temp->back = nullptr;
+        free(temp);
+        return;
+    }
+    prev->next = front;
+    front->back = prev;
+
+    temp->next = temp->back = nullptr;
+    free(temp);
+}
+
+Node* insertBeforeHead(Node* head, int val){
+    Node* newHead = new Node(val, head, nullptr);
+    head->back = newHead;
+
+    return newHead;
+}
+
+Node* insertBeforeTail(Node* head, int val){
+    if(head->next == NULL){
+        return insertBeforeHead(head, val);
+    }
+    Node* tail = head;
+    while(tail->next != NULL){
+        tail = tail->next;
+    }
+
+    Node* prev = tail->back;
+    Node* newNode = new Node(val, tail, prev);
+    prev->next = newNode;
+    tail->back = newNode;
+
+    return head;
+}
+
+Node* insertBeforeKthElement(Node* head, int k, int val){
+    if(k==1){
+        return insertBeforeHead(head, val);
+    }
+    Node* temp = head;
+    int cnt = 0;
+    while(temp!=NULL){
+        cnt++;
+        if(cnt == k) break;
+        temp = temp->next;
+    }
+
+    Node* prev = temp->back;
+    Node* newNode = new Node(val, temp, prev);
+    prev->next = newNode;
+    temp->back = newNode;
+    return  head;
+}
+
+void insertBeforeNode(Node* node, int val){
+    Node* prev = node->back;
+    Node* newNode = new Node(val, node, prev);
+    prev->next = newNode;
+    node->back = newNode;
 }
 
 int main(){
